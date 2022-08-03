6dc0d4a7f328d4ff036124151bf2b37306f3ed6a
Iván Nzohad Casas <100694470+IVI-UNI@users.noreply.github.com>
2022-08-03 22:50:08 +0200

    Añadido código BD final

--- pgScript.sh
+++ pgScript.sh
@@ -97,3 +97,94 @@ psql -U postgres temporal -c "COPY Egresados2020 FROM '$HOME/Practica1/Egresados
 
 psql -U postgres  temporal -c "CREATE TABLE Egresados2019(CURSO_ACADEMICO SMALLINT,LOCALIDAD VARCHAR(100), ESTUDIO VARCHAR(150),TIPO_ESTUDIO VARCHAR(15), TIPO_EGRESO VARCHAR(70), SEXO VARCHAR(10), ALUMNOS_GRADUADOS SMALLINT, ALUMNOS_INTERRUMPEN_STUDIOS INT, ALUMNOS_INTERRUMPEN_EST_ANO1 INT, ALUMNOS_TRASLADAN_OTRA_UNIV INT , DURACION_mEDIA_GRADUADOS INT, TASA_EFIENCIA FLOAT, FECHA_ACTUALIZACION DATE);"
 psql -U postgres temporal -c "COPY Egresados2019 FROM '$HOME/Practica1/Egresados2021.csv' DELIMITER ';'CSV HEADER;"
+
+psql -c "DROP DATABASE IF EXISTS resultados_oferta_academica;"
+psql -c "CREATE DATABASE resultados_oferta_academica;"
+psql resultados_oferta_academica -c "CREATE TABLE Estudio (
+	id_estudio SERIAL ,
+	nombre_estudio varchar(100) NULL,
+	tipo_estudio varchar(15) NULL,
+    PRIMARY KEY(id_estudio)
+);"
+psql resultados_oferta_academica -c "CREATE TABLE Centro (
+	id_centro SERIAL ,
+	nombre_centro varchar(100) NULL,
+	tipo_centro varchar(15) NULL,
+    PRIMARY KEY(id_centro)
+);"
+
+psql resultados_oferta_academica -c "CREATE TABLE Impartido (
+    IDestudio smallint ,	
+    IDcentro smallint,
+    curso smallint,
+	plazas_ofertadas smallint NULL,
+    plazas_matriculadas smallint NULL,
+    plazas_solicitadas int NULL,
+	alumnos_nuevo_ingreso smallint NULL,
+    nota_corte_julio float NULL,
+    nota_corte_septiembre float NULL,
+    alumnos_graduados smallint NULL,
+    alumnos_adapta_grado_matri smallint NULL,
+    alumnos_adapta_grado_matri_ni smallint NULL,
+    alumnos_adapta_grado_titulado smallint NULL,
+    alumnos_con_reconocimiento smallint NULL,  
+    alumnos_movilidad_entrada smallint NULL,
+    alumnos_movilidad_salida smallint NULL,
+    creditos_matriculados float NULL,
+    creditos_reconocidos float NULL,
+    duracion_media_graduados float NULL,
+    egresados_voluntarios smallint NULL,
+    indice_ocupacion float NULL,
+    tasa_exito float NULL,
+    tasa_rendimiento float NULL,
+    tasa_abandono float NULL,
+    tasa_graduacion float NULL,
+    PRIMARY KEY (IDestudio, IDcentro),
+    FOREIGN KEY (IDcentro) REFERENCES Centro(id_centro)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (IDestudio) REFERENCES Estudio(id_estudio)
+	ON DELETE CASCADE ON UPDATE CASCADE
+    
+);"
+
+psql resultados_oferta_academica -c " CREATE TABLE Localidad (
+	id_localidad SERIAL,
+	nombre_localidad varchar(100) NULL,
+    PRIMARY KEY(id_localidad) 
+);"
+
+psql resultados_oferta_academica -c " CREATE TABLE Pais (
+	id_pais SERIAL,
+	nombre_pais varchar(100) NULL,
+    PRIMARY KEY(id_pais) 
+);"
+
+
+psql resultados_oferta_academica -c " CREATE TABLE Movilidad (
+	nombre_movilidad varchar(100) NOT NULL UNIQUE,
+    id_centro smallint NOT NULL,
+    PRIMARY KEY(nombre_movilidad, id_centro),
+    FOREIGN KEY (id_centro) REFERENCES Centro(id_centro)
+	ON DELETE CASCADE ON UPDATE CASCADE    
+);"
+
+
+psql resultados_oferta_academica -c " CREATE TABLE Realiza (
+	curso smallint,
+	nombre_movilidad varchar(100),
+    id_centro smallint,
+    id_pais smallint NOT NULL,
+    area_estudios varchar(100) NULL,
+    in_out varchar(4) NULL,
+    nombre_idioma varchar(50),
+    universidad_destino varchar(100),
+    plazas_ofertadas smallint,
+    plazas_asignadas smallint,
+    PRIMARY KEY (nombre_movilidad, id_centro),
+    FOREIGN KEY (nombre_movilidad) REFERENCES Movilidad(nombre_movilidad)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (id_pais) REFERENCES Pais(id_pais)
+	ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_centro) REFERENCES Centro(id_centro)
+	ON DELETE CASCADE ON UPDATE CASCADE
+  );"
